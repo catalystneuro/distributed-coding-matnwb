17d5bbc8ff293be24d8d226347bb13b22115a7c3
Ben Dichter <ben.dichter@gmail.com>
2021-08-09 11:11:25 -0400

    Update +nxpl2nwb/LickPiezo.m

--- +nxpl2nwb/LickPiezo.m
+++ +nxpl2nwb/LickPiezo.m
@@ -3,8 +3,7 @@ function lp_timeseries = LickPiezo(file_prefix, dataunit, description)
     arguments
         file_prefix (1, :) string = ''
         dataunit char = 'Unknown'
-        description char = {'Voltage values from a thin-film piezo '
-                            'connected to the lick spout'}
+        description char = 'Voltage values from a thin-film piezo connected to the lick spout'
     end
     %% strcat(file_prefix, 'face.motionEnergy.npy')
     fname_lickpiezo_raw = strcat(file_prefix, 'lickPiezo.raw.npy');
